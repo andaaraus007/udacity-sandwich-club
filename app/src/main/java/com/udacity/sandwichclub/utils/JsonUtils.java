1e4620fd87f2e4cc3896db0fcd81ae698a05ce39
eric <and44raus@gmail.com>
2020-06-14 00:43:49 -0700

    Make Scrollview as parent layout so that complete content can be accessed on small screen device and different orientation.

--- app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
+++ app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
@@ -13,16 +13,15 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class JsonUtils {
+    public static final String KEY_NAME = "name";
+    public static final String KEY_MAIN_NAME = "mainName";
+    public static final String KEY_ALSO_KNOWN_AS = "alsoKnownAs";
+    public static final String KEY_PLACE_OF_ORIGIN = "placeOfOrigin";
+    public static final String KEY_DESCRIPTION = "description";
+    public static final String KEY_IMAGE = "image";
+    public static final String KEY_INGREDIENTS = "ingredients";
 
     public static Sandwich parseSandwichJson(String json) throws JSONException {
-        final String KEY_NAME = "name";
-        final String KEY_MAIN_NAME = "mainName";
-        final String KEY_ALSO_KNOWN_AS = "alsoKnownAs";
-        final String KEY_PLACE_OF_ORIGIN = "placeOfOrigin";
-        final String KEY_DESCRIPTION = "description";
-        final String KEY_IMAGE = "image";
-        final String KEY_INGREDIENTS = "ingredients";
-
         Sandwich sandwich = null;
         JSONObject sandwichJsonObj = new JSONObject(json);
 
